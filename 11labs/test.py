fed55b4eb22a685dfdb3141b72b9e11a6808c32f
Maria Eduarda Couto Grossi <maria.grossi1@estudante.ufla.br>
2023-09-21 09:49:38 -0300

    thread ok

--- 11labs/test.py
+++ 11labs/test.py
@@ -1,13 +1,13 @@
 from elevenlabs import clone, generate, play, set_api_key
 from elevenlabs.api import History
 
-set_api_key("eb23a97bf0e94745db15caa71a7fa49e")
+set_api_key("9077d79c1d6b8fffd8e385ad062f8605")
 
 from elevenlabs import generate, play
 
 audio = generate(
   text="Voltei",
-  voice="Daniel",
+  voice="Rachel",
   model="eleven_multilingual_v2"
 )
 
--- controller/control.py
+++ controller/control.py
@@ -3,6 +3,22 @@ import json
 import random
 import threading
 
+class InterfaceThread(threading.Thread):
+    def __init__(self, interface_str):
+        super().__init__()
+        self.interface_str = interface_str
+
+    def run(self):
+        mostrarInterface(self.interface_str)
+
+class FalaThread(threading.Thread):
+    def __init__(self, fala_str):
+        super().__init__()
+        self.fala_str = fala_str
+
+    def run(self):
+        falarComUsuario(self.fala_str)
+
 #funções auxiliares para o uso das threads
 def falarComUsuario(str):
   act.falar(str)
@@ -12,15 +28,20 @@ def mostrarInterface(str):
 
 def runRobot():
 
-  thread_interface = threading.Thread(target=mostrarInterface)
-  thread_fala = threading.Thread(target=falarComUsuario)
+  interface_str = "robot.png"
+  fala_str = "Oi! Tudo bem? Sou seu mais novo amigo robô! Para começarmos, qual animal você deseja que eu seja?"
+
+  thread_interface = InterfaceThread(interface_str)
+  thread_fala = FalaThread(fala_str)
 
   thread_interface.start()
   thread_fala.start()
+
+  thread_fala.join()
   
   #coloca a imagem do robozinho, ele se apresenta e já solta "qual animal você deseja?"
-  mostrarInterface("robot.png")
-  falarComUsuario("Oi! Tudo bem? Sou seu mais novo amigo robô! Para começarmos, qual animal você deseja que eu seja?")
+  #mostrarInterface("robot.png")
+  #falarComUsuario("Oi! Tudo bem? Sou seu mais novo amigo robô! Para começarmos, qual animal você deseja que eu seja?")
   stt = sense.speech_to_text()
   print('STT = ', stt)
 
@@ -56,4 +77,3 @@ def runRobot():
         break
 
   thread_interface.join()
-  thread_fala.join()
